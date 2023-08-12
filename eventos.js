44677337ed73460abacb67062348e734f19385a9
Karen Lewy <karenlewy@gmail.com>
2023-08-12 20:03:17 -0300

    Modificacion

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
+function manejarClick(event) {
+    event.stopPropagation(); // Detener la propagación del evento
+    alert("Hola! Soy el botón.");
+}
+
 const div = document.getElementById("div_container");
 
 function clickDiv() {
